f71ba496390ef5b392b8d36e1de1a2067386b61d
Salik <iamsalikjamal@gmail.com>
2022-06-19 13:57:44 +0500

    project setup on the blockchain side

---
+++ src/contracts/Migrations.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.7.0 < 0.9.0;
+
+
+contract Migrations {
+
+  address public owner = msg.sender;
+  uint public last_completed_migration;
+
+  modifier restricted {
+    require(msg.sender == owner, "This function is restricted to the contract's owner!");
+    _;
+  }
+
+  function setCompleted(uint _completed) public restricted {
+    last_completed_migration = _completed;
+  }
+
+  function upgrade(address _newAddress) public restricted {
+    Migrations upgraded = Migrations(_newAddress);
+    upgraded.setCompleted(last_completed_migration);
+  }
+  
+}
