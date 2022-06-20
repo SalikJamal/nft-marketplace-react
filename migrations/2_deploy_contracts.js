8a4f9e1b24c895bff35bab07d3b909b715c789b8
Salik <iamsalikjamal@gmail.com>
2022-06-20 15:42:56 +0500

    All code till section 10

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+const DarkPhantom = artifacts.require("DarkPhantom")
+
+
+module.exports = deployer => {
+    deployer.deploy(DarkPhantom)
+}
---
+++ src/contracts/DarkPhantom.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+contract DarkPhantom {
+
+    string public name;
+    string public symbol;
+
+    constructor() {
+        name = "Dark Phantom";
+        symbol = "DPhantomz";
+    }
+
+}
