74a8d0f00e481b52550a70a7479b9f29185a7be8
Salik <iamsalikjamal@gmail.com>
2022-06-27 19:11:07 +0500

    Completion of transferFrom function

--- src/contracts/ERC721/ERC721.sol
+++ src/contracts/ERC721/ERC721.sol
@@ -35,10 +35,12 @@ contract ERC721 {
     // Mapping from owner to number of owned tokens
     mapping(address => uint) private ownedTokensCount;
 
+    // Mapping from tokenId to approved addresses
+    mapping(uint => address) private tokenApprovals; 
+
     event Transfer(address indexed _from, address indexed _to, uint indexed tokenId);
 
     function _mint(address _to, uint _tokenId) internal virtual {
-        
         require(_to != address(0), "ERC721: Minting to the zero address"); // Requires that the address isn't invalid
         require(!_tokenExists(_tokenId), "ERC721: Token already minted"); // Requires that the token doesn't already exist
 
@@ -46,9 +48,31 @@ contract ERC721 {
         ownedTokensCount[_to] += 1; // Keeping track of tokens being minted to each address, and adding 1 to it every time it mints
 
         emit Transfer(address(0), _to, _tokenId); // Emit the Transfer event when transfering token to address
-
     }
 
+    /// @notice Transfer ownership of an NFT
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    function _transferFrom(address _from, address _to, uint _tokenId) internal {
+        require(_to != address(0), "ERC721: Transfer to the zero address");
+        require(_tokenExists(_tokenId), "ERC721: Token does not exist");
+        require(ownerOf(_tokenId) == _from, "ERC721: Only the owner of the token can transfer");
+
+        tokenOwners[_tokenId] = _to; // Changing the owner of the token
+        ownedTokensCount[_from] -= 1; // Deduct one token from the owner when transferring
+        ownedTokensCount[_to] += 1; // Add one token to the new owner when transferring
+
+        emit Transfer(_from, _to, _tokenId); // Emit the Transfer event when transfering token to new address
+    }
+
+    function transferFrom(address _from, address _to, uint _tokenId) public {
+        _transferFrom(_from, _to, _tokenId);
+    }
 
     /// @notice Count all NFTs assigned to an owner
     /// @dev NFTs assigned to the zero address are considered invalid, and this function throws for queries about the zero address.
@@ -64,14 +88,15 @@ contract ERC721 {
     /// @dev NFTs assigned to zero address are considered invalid, and queries about them do throw.
     /// @param _tokenId The identifier for an NFT
     /// @return The address of the owner of the NFT
-    function ownerOf(uint _tokenId) external view returns (address) {
+    function ownerOf(uint _tokenId) public view returns (address) {
         address owner = tokenOwners[_tokenId];
         require(owner != address(0), "Owner query for non-existant token");
         return owner;
     }
 
     function _tokenExists(uint _tokenId) internal view returns(bool) {
-        address owner = tokenOwners[_tokenId]; // Setting the address of nft owner to check the mapping of the address from tokenOwner at the tokenId
+        // Setting the address of nft owner to check the mapping of the address from tokenOwner at the tokenId
+        address owner = tokenOwners[_tokenId];
         return owner != address(0); // Return truthiness that address is not zero
     }
 
